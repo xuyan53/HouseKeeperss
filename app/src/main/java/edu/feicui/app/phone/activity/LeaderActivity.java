8a6164ba0f749f5ed475db6b6b685e76a675fc4f
xuyan53 <676986460@qq.com>
2016-12-27 14:49:35 +0800

    animation

--- app/src/main/java/edu/feicui/app/phone/activity/LeaderActivity.java
+++ app/src/main/java/edu/feicui/app/phone/activity/LeaderActivity.java
@@ -1,9 +1,12 @@
 package edu.feicui.app.phone.activity;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
 import android.view.animation.AnimationUtils;
 import android.view.animation.RotateAnimation;
 import android.view.animation.ScaleAnimation;
@@ -14,7 +17,7 @@ import edu.feicui.app.phone.R;
 
 public class LeaderActivity extends AppCompatActivity {
     ImageView mImgScale,mImgAlpha,mImgRotate,mImgTranslate;
-
+AnimationDrawable anim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +28,14 @@ public class LeaderActivity extends AppCompatActivity {
         mImgAlpha=(ImageView) findViewById(R.id.iv_alpha);
         mImgRotate=(ImageView) findViewById(R.id.iv_rotate);
         mImgTranslate=(ImageView) findViewById(R.id.iv_translate);
-        Animation animScale= AnimationUtils.loadAnimation(LeaderActivity.this,R.anim.scale);
-        mImgScale.startAnimation(animScale);
+           mImgScale.setImageResource(R.drawable.zhen);
+        anim=(AnimationDrawable) mImgScale.getDrawable();
+        anim.start();
+        anim.stop();
+
+
+//        Animation animScale= AnimationUtils.loadAnimation(LeaderActivity.this,R.anim.any);
+//        mImgScale.startAnimation(animScale);
 
 //        Animation animAlpha= AnimationUtils.loadAnimation(LeaderActivity.this,R.anim.alpha);
 //        mImgAlpha.startAnimation(animAlpha);
@@ -38,6 +47,15 @@ public class LeaderActivity extends AppCompatActivity {
 //        ScaleAnimation scaleAnimation=new ScaleAnimation(0f,5.4f,0f,5.4f,0.5f,0.5f);
 //        scaleAnimation.setDuration(5000);
 //        scaleAnimation.setRepeatCount(Animation.INFINITE);
+//        AlphaAnimation alphaAnimation=new AlphaAnimation(0,1);
+//          alphaAnimation.setRepeatCount(Animation.INFINITE);
+//
+//        AnimationSet set=new AnimationSet(true);
+//        set.addAnimation(scaleAnimation);
+//        set.addAnimation(alphaAnimation);
+//        set.setDuration(5000);
+//
+//        mImgScale.startAnimation(set);
 //        mImgScale.startAnimation(scaleAnimation);
 //        AlphaAnimation alphaAnimation=new AlphaAnimation(0,1);
 //        alphaAnimation.setDuration(5000);
